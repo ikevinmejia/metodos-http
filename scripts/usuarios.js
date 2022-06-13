a4f280208a6be11bfcedd1ed5ba51982499cf6c7
ikevinmejia <kevin_6958@hotmail.com>
2022-06-13 16:00:50 -0500

    update usuario js

--- scripts/usuarios.js
+++ scripts/usuarios.js
@@ -1,2 +1,9 @@
 const form = document.querySelector('#formulario');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#name').value;
+    const apellido = document.querySelector('#lastName').value;
+    const correo = document.querySelector('#email').value;
+})
